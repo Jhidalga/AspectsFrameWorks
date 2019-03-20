e42bf64bb5edecd003ac182b7699e7f490a019ef
BlueIndio <34249813+BlueIndio@users.noreply.github.com>
2019-03-20 20:34:35 +0100

    Tiene conflictos de versiones

---
+++ MyService.Application/Interceptors/HelloWorldInterceptor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Castle.DynamicProxy;
+
+namespace MyService.Application.Interceptors
+{
+    public class HelloWorldInterceptor : Snap.MethodInterceptor
+    {
+        public override void InterceptMethod(IInvocation invocation, MethodBase method, Attribute attribute)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+//CointainerConfiguration
+
+//SnapConfiguration.For<StructureMapAspectContainer>(c =>
+//            {
+//                // Tell SNAP to only be concerned with stuff starting with this namespace.
+//                c.IncludeNamespace("MyService.Application*");
+//                // Tell SNAP to intercept any method or class decorated with "DemoLoggingAttribute"
+//                // by wrapping execution in your own DemoInterceptor class.
+//                c.Bind<DemoLoggingInterceptor>().To<DemoLoggingAttribute>();
+//            });
